7377138707e83ddf8a822360bddebcc3e998f6c1
Dominique Nguyen <don010@UCSD.EDU>
2017-11-14 11:06:42 -0800

    Small Fix

--- ConsoleApp1/Properties/Settings.Designer.cs
+++ ConsoleApp1/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WhalesFargo.Properties {
+namespace ConsoleApp1.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
