931dcb9a1345a2f824954f793de22de3d0fab5f6
Sebastian Dang <s1dang@ucsd.edu>
2017-11-20 10:10:35 -0800

    Added a help module, help version 2. Will change to this version in the future.

--- src/AudioModule.cs
+++ src/AudioModule.cs
@@ -11,6 +11,7 @@ namespace WhalesFargo
      * Class that handles the audio portion of the program.
      * An audio module is created here with commands that interact with an AudioService.
      */
+    [Summary("Audio class to interact with voice chat. Currently, used to playback audio in a stream.")]
     public class AudioModule : ModuleBase
     {
         // Private variables
@@ -48,6 +49,7 @@ namespace WhalesFargo
         // instead, outsource to a service for that.'
 
         [Command("join", RunMode = RunMode.Async)]
+        [Summary("Joins the user's voice channel.")]
         public async Task JoinVoiceChannel()
         {
             if (m_Service.GetDelayJoin()) return; // Stop multiple attempts to join too quickly.
@@ -55,12 +57,14 @@ namespace WhalesFargo
         }
 
         [Command("leave", RunMode = RunMode.Async)]
+        [Summary("Leaves the current voice channel.")]
         public async Task LeaveVoiceChannel()
         {
             await m_Service.LeaveAudio(Context.Guild);
         }
 
         [Command("play", RunMode = RunMode.Async)]
+        [Summary("Plays a song by url or local path.")]
         public async Task PlayVoiceChannel([Remainder] string song)
         {
             // Extract the audio. Download here if necessary. TODO: Catch if youtube-dl can't read the header.
@@ -76,6 +80,7 @@ namespace WhalesFargo
         }
 
         [Command("pause", RunMode = RunMode.Async)]
+        [Summary("Pauses the current song, if playing.")]
         public async Task PauseVoiceChannel()
         {
             m_Service.PauseAudio();
@@ -83,6 +88,7 @@ namespace WhalesFargo
         }
 
         [Command("resume", RunMode = RunMode.Async)]
+        [Summary("Pauses the current song, if paused.")]
         public async Task ResumeVoiceChannel()
         {
             m_Service.ResumeAudio();
@@ -90,6 +96,7 @@ namespace WhalesFargo
         }
 
         [Command("stop", RunMode = RunMode.Async)]
+        [Summary("Stops the current song, if playing or paused.")]
         public async Task StopVoiceChannel()
         {
             m_Service.StopAudio();
@@ -97,6 +104,7 @@ namespace WhalesFargo
         }
 
         [Command("volume")]
+        [Summary("Changes the volume to [0.0, 1.0].")]
         public async Task VolumeVoiceChannel([Remainder] float volume)
         {
             m_Service.AdjustVolume(volume);
@@ -104,6 +112,7 @@ namespace WhalesFargo
         }
 
         [Command("add", RunMode = RunMode.Async)]
+        [Summary("Adds a song by url or local path to the playlist.")]
         public async Task AddVoiceChannel([Remainder] string song)
         {
             await m_Service.PlaylistAdd(song);
@@ -115,6 +124,7 @@ namespace WhalesFargo
         }
 
         [Command("skip", RunMode = RunMode.Async)]
+        [Summary("Skips the current song, if playing from the playlist.")]
         public async Task SkipVoiceChannel()
         {
             m_Service.PlaylistSkip();
@@ -122,12 +132,14 @@ namespace WhalesFargo
         }
 
         [Command("playlist", RunMode = RunMode.Async)]
+        [Summary("Shows what's currently in the playlist.")]
         public async Task PrintPlaylistVoiceChannel()
         {
             await ServiceReplyAsync(m_Service.PlaylistString()); // Reply with a print out.
         }
 
         [Command("autoplay", RunMode = RunMode.Async)]
+        [Summary("Starts the autoplay service on the current playlist.")]
         public async Task AutoPlayVoiceChannel([Remainder] bool enable)
         {
             // Start the autoplay service. This function is BLOCKING.
---
+++ src/HelpModule.cs
@@ -0,0 +1,97 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhalesFargo
+{
+    /**
+     * HelpModule
+     * Class that handles the help functionality of the program.
+     * This searches for all modules and prints out it's 'summary' tag.
+     * If you search by module, it will print out all it's commands by it's 'summary' tag.
+     */
+    public class HelpModule : ModuleBase
+    {
+        private readonly CommandService m_Commands;
+        private readonly IServiceProvider m_Provider;
+
+        public HelpModule(CommandService commands, IServiceProvider provider)
+        {
+            m_Commands = commands;
+            m_Provider = provider;
+        }
+
+        [Command("help2", RunMode = RunMode.Async)]
+        [Summary("Finds all the modules and prints out it's summary tag.")]
+        public async Task HelpAsync()
+        {
+            // Get all the modules.
+            var modules = m_Commands.Modules.Where(x => !string.IsNullOrWhiteSpace(x.Summary));
+
+            // Create an embed builder.
+            var emb = new EmbedBuilder();
+
+            // For each module...
+            foreach (var module in modules)
+            {
+                bool success = false;
+                foreach (var command in module.Commands)
+                {
+                    var result = await command.CheckPreconditionsAsync(Context, m_Provider);
+                    if (result.IsSuccess)
+                    {
+                        success = true;
+                        break;
+                    }
+                }
+                if (!success)
+                    continue;
+
+                emb.AddField(module.Name, module.Summary);
+            }
+
+            await ReplyAsync("", false, emb);
+        }
+
+        [Command("help2", RunMode = RunMode.Async)]
+        [Summary("Finds all the commands from a specific module and prints out it's summary tag.")]
+        public async Task HelpAsync(string moduleName)
+        {
+            // Get the module in question.
+            var module = m_Commands.Modules.FirstOrDefault(x => x.Name.ToLower() == moduleName.ToLower());
+
+            // If null, we chose a bad module.
+            if (module == null)
+            {
+                await ReplyAsync($"The module `{moduleName}` does not exist.");
+                return;
+            }
+
+            // Find all it's commands.
+            var commands = module.Commands.Where(x => !string.IsNullOrWhiteSpace(x.Summary)).GroupBy(x => x.Name).Select(x => x.First());
+
+            // If none of them have summaries or don't exist, return.
+            if (commands.Count() == 0)
+            {
+                await ReplyAsync($"The module `{module.Name}` has no available commands :(");
+                return;
+            }
+
+            // Create an embed builder.
+            var emb = new EmbedBuilder();
+
+            // For each command...
+            foreach (var command in commands)
+            {
+                var result = await command.CheckPreconditionsAsync(Context, m_Provider);
+                if (result.IsSuccess)
+                    emb.AddField(command.Aliases.First(), command.Summary);
+            }
+
+            await ReplyAsync("", false, emb);
+        }
+
+    }
+}
