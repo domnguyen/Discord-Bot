e73dffd48748875dbc3dd660fc5857724b51e685
Sebastian Dang <s1dang@ucsd.edu>
2018-04-20 12:45:09 -0700

    Cleaned up for release.

---
+++ src/Helpers/Tools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace WhalesFargo
+{
+    public static class Tools
+    {
+        // Check if a process is running or not.
+        public static bool IsRunning(this Process process)
+        {
+            try { Process.GetProcessById(process.Id); }
+            catch (InvalidOperationException) { return false; }
+            catch (ArgumentException) { return false; }
+            return true;
+        }
+    }
+}
