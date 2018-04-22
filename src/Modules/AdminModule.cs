2910b5a8a8af2af1bc221d88d96c72dfbc4aee66
Sebastian Dang <s1dang@ucsd.edu>
2018-04-21 21:42:22 -0700

    some small fixes.

--- src/Modules/AdminModule.cs
+++ src/Modules/AdminModule.cs
@@ -2,10 +2,11 @@
 using Discord;
 using Discord.Commands;
 using WhalesFargo.Services;
-using Discord.WebSocket;
 
 namespace WhalesFargo.Modules
 {
+    [Name("Admin")]
+    [Summary("Admin module to manage this discord server.")]
     public class AdminModule : CustomModule
     {
         // Private variables
@@ -25,9 +26,9 @@ namespace WhalesFargo.Modules
         [Summary("This allows admins to mute users.")]
         [RequireUserPermission(GuildPermission.Administrator)]
         [RequireUserPermission(GuildPermission.MuteMembers)]
-        public async Task MuteUser([Remainder] IGuildUser user = null)
+        public async Task MuteUser([Remainder] IGuildUser user)
         {
-            await m_Service.MuteUser(Context.Guild, Context.User);
+            await m_Service.MuteUser(Context.Guild, user);
         }
 
         [Command("unmute")]
@@ -35,16 +36,16 @@ namespace WhalesFargo.Modules
         [Summary("This allows admins to unmute users.")]
         [RequireUserPermission(GuildPermission.Administrator)]
         [RequireUserPermission(GuildPermission.MuteMembers)]
-        public async Task UnmuteUser([Remainder] IGuildUser user = null)
+        public async Task UnmuteUser([Remainder] IGuildUser user)
         {
-            await m_Service.UnmuteUser(Context.Guild, Context.User);
+            await m_Service.UnmuteUser(Context.Guild, user);
         }
 
         [Command("kick")]
         [Remarks("!kick [user]")]
         [Summary("This allows admins to kick.")]
         [RequireUserPermission(GuildPermission.Administrator)]
-        public async Task KickUser(IGuildUser user = null, [Remainder] string reason = null)
+        public async Task KickUser(IGuildUser user, [Remainder] string reason = null)
         {
             await m_Service.KickUser(Context.Guild, user, reason);
         }
@@ -53,17 +54,17 @@ namespace WhalesFargo.Modules
         [Remarks("!addrole [user]")]
         [Summary("This allows admins to add specific roles to a user.")]
         [RequireUserPermission(GuildPermission.Administrator)]
-        public async Task AddRoleUser(IGuildUser user = null, [Remainder]string role = null)
+        public async Task AddRoleUser(IGuildUser user, [Remainder]string role)
         {
             await m_Service.AddRoleUser(Context.Guild, user, role);
         }
 
         [Command("removerole")]
         [Alias("removerole", "delrole")]
-        [Remarks("!removerole [user]")]
+        [Remarks("!delrole [user]")]
         [Summary("This allows admins to remove specific roles to a user.")]
         [RequireUserPermission(GuildPermission.Administrator)]
-        public async Task RemoveRoleUser(IGuildUser user = null, [Remainder]string role = null)
+        public async Task RemoveRoleUser(IGuildUser user, [Remainder]string role)
         {
             await m_Service.RemoveRoleUser(Context.Guild, user, role);
         }
