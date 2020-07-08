4675153fc9bd3f7bd7984830357f11d15c2c87e8
Sebastian Dang <s1dang@ucsd.edu>
2020-07-07 21:55:37 -0700

    Add basic config helper class. Updated json lib.

---
+++ src/Helpers/Config.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace WhalesFargo.Helpers
+{
+    /**
+    * Config
+    * Singleton class that handles basic configuration parameters for the app.
+    * This class is not thread-safe.
+    * TODO: Convert to thread-safe solution
+    */
+    public sealed class Config
+    {
+        private static Config m_Instance;
+        private static string m_FileName;
+
+        private Config() { }
+
+        public static Config Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                    m_Instance = new Config();
+                return m_Instance;
+            }
+        }
+
+        public void Read(string filename)
+        {
+            if (!File.Exists(filename)) return;
+            if (m_Instance == null) return;
+            m_FileName = filename;
+            JsonConvert.PopulateObject(File.ReadAllText(filename), m_Instance);
+        }
+
+        public void Write()
+        {
+            File.WriteAllText(m_FileName, JsonConvert.SerializeObject(m_Instance));
+        }
+
+        [JsonProperty]
+        public string ApiKey { get; set; }
+
+        [JsonProperty]
+        public string DiscordToken { get; set; }
+
+        [JsonProperty]
+        public char Prefix { get; set; }
+    }
+}
